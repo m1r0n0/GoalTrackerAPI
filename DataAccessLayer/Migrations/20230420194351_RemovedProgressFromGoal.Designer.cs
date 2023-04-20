3b504086dedd8eb22a2fc3f13e184c7c5d937475
m1r0n0 <mironuk29@gmail.com>
2023-04-20 22:50:17 +0300

    removed Progress property DB Migration

--- DataAccessLayer/Migrations/20230420194351_RemovedProgressFromGoal.Designer.cs
+++ DataAccessLayer/Migrations/20230420194351_RemovedProgressFromGoal.Designer.cs
@@ -4,6 +4,7 @@ using DataAccessLayer.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class GoalContextModelSnapshot : ModelSnapshot
+    [Migration("20230420194351_RemovedProgressFromGoal")]
+    partial class RemovedProgressFromGoal
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
